95eb1ea3a6fb3bf746c74d6a0b7ff0e807e08a66
友近武史 <tomochikatakeshi@tomochikatakeshinoMacBook-Air.local>
2023-09-24 18:22:05 +0900

    田中ごちに

--- bmi.py
+++ bmi.py
@@ -3,12 +3,19 @@ class BMI:
         self.height = height
         self.weight = weight
 
-    def calucurate_bmi(self):
+    def calculate_bmi(self):
         return round(self.weight/self.height**2, 2)
 
 
 tanaka_bmi = BMI(height = 1.80, weight = 67.0)
 sasami_bmi = BMI(height = 1.58, weight = 80.0)
 
-print(tanaka_bmi.calucurate_bmi())
-print(sasami_bmi.calucurate_bmi())
+ # tanakaさんの情報
+print("tanaka")
+print(tanaka_bmi.height,tanaka_bmi.weight)
+print(tanaka_bmi.calculate_bmi())
+
+# sasamiさんの情報
+print("sasami")
+print(sasami_bmi.height,sasami_bmi.weight)
+print(sasami_bmi.calculate_bmi())
